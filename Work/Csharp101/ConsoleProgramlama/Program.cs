d97251063a4dec2846b3cbc18f6ddfaeea9e2b9b
Cem <cicekcem25@gmail.com>
2021-09-01 18:40:47 +0300

    ConsoleProgramlama

---
+++ Work/Csharp101/ConsoleProgramlama/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleProgramlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("İsminizi Giriniz:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyisminizi Giriniz:");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Merhaba " + name + " " + surname);
+            Console.ReadKey();
+            
+        }
+    }
+}
