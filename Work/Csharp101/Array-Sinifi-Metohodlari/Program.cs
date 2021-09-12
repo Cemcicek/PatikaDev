358fb45a6ce7653f1b141d4d86b92def8692db5b
Cem <cicekcem25@gmail.com>
2021-09-12 22:53:38 +0300

    array-sinif-methodlari

---
+++ Work/Csharp101/Array-Sinifi-Metohodlari/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Array_Sinifi_Metohodlari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] sayiDizisi = { 23, 12, 4, 86, 72, 3, 11, 17 };
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+            // Sort
+            Console.WriteLine("---------Array Sort---------");
+            Array.Sort(sayiDizisi);
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+            // Clear
+            Console.WriteLine("---------Array Clear---------");
+            Array.Clear(sayiDizisi, 2, 2);//sayiDizisi elemanlarını kullanarak 2.index ten itibaren 2 tane elemanı sıfırlar.(2.index dahil)
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+            // Reverse
+            Console.WriteLine("---------Array Reverse---------");
+            Array.Reverse(sayiDizisi);
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+            // IndexOf
+            Console.WriteLine("---------Array IndexOf---------");
+            Console.WriteLine(Array.IndexOf(sayiDizisi, 23));
+
+            // Resize
+            Console.WriteLine("---------Array Resize---------");
+            Array.Resize<int>(ref sayiDizisi, 9);
+            sayiDizisi[8] = 99;
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+        }
+    }
+}
