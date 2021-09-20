d9dbea25b434db7119f7e799d6823aa166acad44
Cem <cicekcem25@gmail.com>
2021-09-20 20:54:47 +0300

    inheritance

---
+++ Work/Csharp101/Inheritance/Canlilar.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Inheritance
+{
+    public class Canlilar
+    {
+        protected void Beslenme()
+        {
+            Console.WriteLine("Canlılar beslenir.");
+        }
+        protected void Solunum()
+        {
+            Console.WriteLine("Canlılar solunum yapar.");
+
+        }
+        protected void Bosaltim()
+        {
+            Console.WriteLine("Canlılar boşaltım yapar.");
+        }
+
+    }
+}
