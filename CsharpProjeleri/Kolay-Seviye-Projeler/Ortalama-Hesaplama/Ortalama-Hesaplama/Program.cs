97153b9257907c349e34749a926ec53fad970489
Cem <cicekcem25@gmail.com>
2022-11-13 20:12:32 +0300

    csharp-project

---
+++ CsharpProjeleri/Kolay-Seviye-Projeler/Ortalama-Hesaplama/Ortalama-Hesaplama/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ortalama_Hesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sayı Giriniz: ");
+            int sayi = Int32.Parse(Console.ReadLine());
+            Fibonacci(sayi);
+        }
+        
+        public static void Fibonacci(int len) 
+        {
+            int a = 0, b = 1, c = 0;
+            Console.Write("{0} {1}", a, b);
+            for (int i = 2; i < len; i++)
+            {
+                c = a + b;
+                Console.Write("{0}", c);
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
