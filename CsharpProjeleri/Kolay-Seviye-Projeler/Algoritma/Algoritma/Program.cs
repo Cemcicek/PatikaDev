687ef9484af35473fc41895d44e19ef1b7755232
Cem <cicekcem25@gmail.com>
2022-11-15 23:02:50 +0300

    algoritma

---
+++ CsharpProjeleri/Kolay-Seviye-Projeler/Algoritma/Algoritma/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Algoritma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen bir kelime giriniz ve aralarında virgül kullanarak çıkarmak istediğiniz karakterin index' ini girin: ");
+            string inpt = Console.ReadLine();
+            string[] input = inpt.Split(",");
+            string kelime = input[0];
+            int cevir = Convert.ToInt32(input[1]);
+            string sonuc = kelime.Remove(cevir, 1);
+            Console.WriteLine(sonuc);
+            Console.ReadLine();
+        }
+    }
+}
