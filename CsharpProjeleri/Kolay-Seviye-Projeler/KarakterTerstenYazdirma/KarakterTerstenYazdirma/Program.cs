4f28b039cc5612278d03c622eb5be6fb94c9494e
Cem <cicekcem25@gmail.com>
2022-11-19 23:34:24 +0300

    karakter-tersten-yazdirma

---
+++ CsharpProjeleri/Kolay-Seviye-Projeler/KarakterTerstenYazdirma/KarakterTerstenYazdirma/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace KarakterTerstenYazdirma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen bir cümle giriniz: ");
+            string sentence = Console.ReadLine();
+
+            char[] character = sentence.ToCharArray();
+            string reverse = string.Empty;
+
+            for (int i = character.Length - 1; i > -1; i--)
+            {
+                reverse += character[i];
+            }
+            Console.WriteLine(reverse);
+        }
+    }
+}
