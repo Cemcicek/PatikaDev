d16d7e1e6127c401f8172bd35b507797e235c038
Cem <cicekcem25@gmail.com>
2022-11-24 21:56:06 +0300

    karakter-degistitme

---
+++ CsharpProjeleri/Orta-Seviye-Projeler/KarakterDegistirme/KarakterDegistirme/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace KarakterDegistirme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen kelimelerin arasına boşluk bırakarak giriniz: ");
+            string kelimeler = Console.ReadLine();
+            string[] kelime = kelimeler.Split();
+
+            foreach (var item in kelime)
+            {
+                char[] karakter = item.ToCharArray();
+                char hafiza = karakter[0];
+                karakter[0] = karakter[karakter.Length - 1];
+                karakter[karakter.Length - 1] = hafiza;
+
+                foreach (var items in karakter)
+                {
+                    string yeniKelime = items.ToString();
+                    Console.Write(yeniKelime);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
